45e008fdbcc97f3941b680d29a6eef1a0b18f275
Matheus <matheusfelipeferreiramartins@gmail.com>
2021-07-04 17:57:28 -0300

    add pagination for all pokemons

--- src/pages/Pokemons.vue
+++ src/pages/Pokemons.vue
@@ -1,15 +1,29 @@
 <template>
   <div>
+    <v-pagination
+      color="secondary"
+      circle
+      v-model="page"
+      :length="totalPages"
+      :total-visible="7"
+    ></v-pagination>
     <PokemonCard
       v-for="pokemon in pokemons"
       :key="pokemon.id"
       :pokemon="pokemon"
     />
+    <v-pagination
+      color="secondary"
+      circle
+      v-model="page"
+      :length="totalPages"
+      :total-visible="7"
+    ></v-pagination>
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from "vue-property-decorator";
+import { Component, Vue, Watch } from "vue-property-decorator";
 import PokemonCard from "@/components/Pokemons/PokemonCard.vue";
 import { PokemonsList } from "@/models/PokemonsList/PokemonsList";
 import { Pokemon } from "../models/Pokemons/Pokemon";
@@ -23,6 +37,8 @@ import { getPokemons, getPokemonsByName } from "../service/pokemons";
 export default class Pokemons extends Vue {
   public offset = 0;
   public limit = 20;
+  public page = 1;
+  public totalPages = 1;
   public pokemonsList: PokemonsList | undefined;
   public pokemons: Pokemon[] = [];
 
@@ -30,9 +46,16 @@ export default class Pokemons extends Vue {
     await this.reloadPokemons();
   }
 
+  @Watch("page")
   private async reloadPokemons() {
+    this.offset = this.limit * (this.page - 1);
+    console.log("PAGE => ", this.page);
+    console.log("OFFSET => ", this.offset);
+    console.log("LIMIT => ", this.limit);
+    this.pokemons = [];
     const response = await getPokemons(this.offset, this.limit);
     if (response) {
+      this.totalPages = Math.ceil(response.count / this.limit);
       this.pokemonsList = response;
       this.loadAllPokemons();
     }
@@ -46,6 +69,7 @@ export default class Pokemons extends Vue {
           this.pokemons.push(pokemonFetched);
         }
       }
+      this.pokemonsList.results = [];
     }
   }
 }
