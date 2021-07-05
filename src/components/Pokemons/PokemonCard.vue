67319f5611ea15ba1709344b01c2085e67db7b11
Matheus <matheusfelipeferreiramartins@gmail.com>
2021-07-05 18:25:55 -0300

    adds pokemons image

--- src/components/Pokemons/PokemonCard.vue
+++ src/components/Pokemons/PokemonCard.vue
@@ -1,16 +1,61 @@
 <template>
   <div>
-    {{ pokemon.name }}
+    <div v-if="pokemon">
+      {{ pokemon.name }}
+      <v-img
+        :lazy-src="getPokemonImg(pokemon.sprites.other)"
+        max-height="300"
+        max-width="421"
+        :src="getPokemonImg(pokemon.sprites.other)"
+      ></v-img>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Prop, Vue } from "vue-property-decorator";
 import { Pokemon } from "@/models/Pokemons/Pokemon";
+import { Result } from "@/models/PokemonsList/Result";
+import { getPokemonsByName } from "@/service/pokemons";
+import { Other } from "@/models/Pokemons/Other";
 
 @Component
 export default class PokemonCard extends Vue {
   @Prop()
-  public pokemon!: Pokemon;
+  public pokemonResult!: Result;
+  public pokemon: Pokemon | null = null;
+
+  private mounted() {
+    this.loadPokemon();
+  }
+
+  private getPokemonImg(other: Other): string {
+    if (other.dream_world.front_default) {
+      return other.dream_world.front_default;
+    }
+    const officialArtwork = "front_default";
+    const jumpChars = 3;
+    const otherString = JSON.stringify(other);
+    const index = otherString.lastIndexOf(officialArtwork);
+    const partialUrl = otherString.substring(
+      index + jumpChars + officialArtwork.length,
+      otherString.length
+    );
+    const indexSufix = partialUrl.indexOf('"');
+    if (indexSufix === -1) {
+      return require("@/assets/NoPokemon.png");
+    }
+    const url = partialUrl.substring(0, indexSufix);
+    return url;
+  }
+
+  private async loadPokemon() {
+    if (this.pokemonResult.name) {
+      const pokemonFetched = await getPokemonsByName(this.pokemonResult.name);
+      if (pokemonFetched) {
+        this.pokemon = pokemonFetched;
+      }
+    }
+  }
 }
 </script>
--- src/pages/Pokemons.vue
+++ src/pages/Pokemons.vue
@@ -1,24 +1,26 @@
 <template>
   <div>
-    <v-pagination
-      color="secondary"
-      circle
-      v-model="page"
-      :length="totalPages"
-      :total-visible="7"
-    ></v-pagination>
-    <PokemonCard
-      v-for="pokemon in pokemons"
-      :key="pokemon.id"
-      :pokemon="pokemon"
-    />
-    <v-pagination
-      color="secondary"
-      circle
-      v-model="page"
-      :length="totalPages"
-      :total-visible="7"
-    ></v-pagination>
+    <div v-if="pokemonsList">
+      <v-pagination
+        color="secondary"
+        circle
+        v-model="page"
+        :length="totalPages"
+        :total-visible="7"
+      ></v-pagination>
+      <PokemonCard
+        v-for="pokemon in pokemonsList.results"
+        :key="pokemon.name"
+        :pokemonResult="pokemon"
+      />
+      <v-pagination
+        color="secondary"
+        circle
+        v-model="page"
+        :length="totalPages"
+        :total-visible="7"
+      ></v-pagination>
+    </div>
   </div>
 </template>
 
@@ -26,8 +28,7 @@
 import { Component, Vue, Watch } from "vue-property-decorator";
 import PokemonCard from "@/components/Pokemons/PokemonCard.vue";
 import { PokemonsList } from "@/models/PokemonsList/PokemonsList";
-import { Pokemon } from "../models/Pokemons/Pokemon";
-import { getPokemons, getPokemonsByName } from "../service/pokemons";
+import { getPokemons } from "../service/pokemons";
 
 @Component({
   components: {
@@ -39,8 +40,7 @@ export default class Pokemons extends Vue {
   public limit = 20;
   public page = 1;
   public totalPages = 1;
-  public pokemonsList: PokemonsList | undefined;
-  public pokemons: Pokemon[] = [];
+  public pokemonsList: PokemonsList | null = null;
 
   private async mounted() {
     await this.reloadPokemons();
@@ -49,27 +49,10 @@ export default class Pokemons extends Vue {
   @Watch("page")
   private async reloadPokemons() {
     this.offset = this.limit * (this.page - 1);
-    console.log("PAGE => ", this.page);
-    console.log("OFFSET => ", this.offset);
-    console.log("LIMIT => ", this.limit);
-    this.pokemons = [];
     const response = await getPokemons(this.offset, this.limit);
     if (response) {
       this.totalPages = Math.ceil(response.count / this.limit);
       this.pokemonsList = response;
-      this.loadAllPokemons();
-    }
-  }
-
-  private async loadAllPokemons() {
-    if (this.pokemonsList?.results) {
-      for (let result of this.pokemonsList.results) {
-        const pokemonFetched = await getPokemonsByName(result.name);
-        if (pokemonFetched) {
-          this.pokemons.push(pokemonFetched);
-        }
-      }
-      this.pokemonsList.results = [];
     }
   }
 }
